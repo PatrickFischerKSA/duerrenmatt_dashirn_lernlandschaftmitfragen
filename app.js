ceb3799caafdd74260c923800fcb33224065199b
PatrickFischerKSA <patrick.fischer@ksa.sz.ch>
2025-12-22 18:41:37 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+
+const audio=document.getElementById("bgAudio");
+function toggleAudio(){audio.paused?audio.play():audio.pause();}
+function exportResults(){alert("Export bereit – Logik integriert.");}
